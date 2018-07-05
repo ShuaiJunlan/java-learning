4f5bbf796631eada9d773814d85d95a5e95387a7
Shuai JUnlan <shuaijunlan@gmail.com>
2018-07-05 21:26:51 +0800

    update

--- Multithread-J/src/main/java/com/sh/thread/daemontest/TestDaemon.java
+++ Multithread-J/src/main/java/com/sh/thread/daemontest/TestDaemon.java
@@ -6,34 +6,34 @@ import org.junit.Test;
  * @author Junlan Shuai[shuaijunlan@gmail.com].
  * @date Created on 7:45 PM 2018/07/05.
  */
-    public class TestDaemon {
-        @Test
-        public void test() {
-            Thread thread = new Thread(() -> {
-                try {
-                    Thread.sleep(1000);
-                    System.out.println("hello");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-            // default false
-            thread.setDaemon(false);
-            thread.start();
-        }
-        public static void main(String[] args) {
-
-            Thread thread = new Thread(() -> {
-                try {
-                    Thread.sleep(1000);
-                    System.out.println("hello");
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            });
-            // default false
-            thread.setDaemon(false);
-            thread.start();
-
-        }
+public class TestDaemon {
+    @Test
+    public void test() {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("hello");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        // default false
+        thread.setDaemon(false);
+        thread.start();
     }
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("hello");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        // default false
+        thread.setDaemon(false);
+        thread.start();
+
+    }
+}
