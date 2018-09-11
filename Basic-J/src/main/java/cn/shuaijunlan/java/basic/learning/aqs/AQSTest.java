76233d19ac4669fd9e431bf77dc762be2ad1b190
Shuai Junlan <shuaijunlan@gmail.com>
2018-09-11 21:29:54 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/aqs/AQSTest.java
@@ -0,0 +1,17 @@
+package cn.shuaijunlan.java.basic.learning.aqs;
+
+import org.junit.Test;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 8:50 PM 2018/09/11.
+ */
+public class AQSTest {
+    @Test
+    public void test1(){
+        // AbstractQueuedSynchronizer
+
+    }
+}
