d11924a989b0907b733d66bb657ba3876b684267
Shuai Junlan <shuaijunlan@gmail.com>
2018-09-19 17:21:58 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/integercache/IntegerCacheTest.java
@@ -0,0 +1,17 @@
+package cn.shuaijunlan.java.basic.learning.integercache;
+
+import org.junit.Test;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 4:30 PM 2018/09/19.
+ */
+public class IntegerCacheTest {
+    @Test
+    public void test1(){
+        Integer a = 100, b = 100;
+        System.out.println(a == b);
+        Integer c = 300, d = 300;
+        System.out.println(c == d);
+    }
+}
---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/integercache/LongCacheTest.java
@@ -0,0 +1,17 @@
+package cn.shuaijunlan.java.basic.learning.integercache;
+
+import org.junit.Test;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 4:27 PM 2018/09/19.
+ */
+public class LongCacheTest {
+    @Test
+    public void test1(){
+        Long a = 100L, b = 100L;
+        System.out.println(a == b);
+        Long c = 300L, d = 300L;
+        System.out.println(c == d);
+    }
+}
