137540aa832d8684ac18adb549e98054a3ffccde
Shuai Junlan <shuaijunlan@gmail.com>
2018-09-22 15:40:37 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/executeservice/ExecuteServiceTest.java
@@ -0,0 +1,28 @@
+package cn.shuaijunlan.java.basic.learning.executeservice;
+
+import org.junit.Test;
+
+import java.util.concurrent.*;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 8:07 PM 2018/09/19.
+ */
+public class ExecuteServiceTest {
+    /**
+     * Testing difference between execute() and submit()
+     */
+    @Test
+    public void test1(){
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.execute(() -> System.out.println("Hello"));
+        Future<String> stringFuture = executorService.submit(() -> "Hello");
+        try {
+            System.out.println(stringFuture.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
