8e46d49c07d16e1ba78824435dbfac6fc45e581e
Shuai Junlan <shuaijunlan@gmail.com>
2018-09-28 10:39:31 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/floattype/FloatValueTest.java
@@ -0,0 +1,15 @@
+package cn.shuaijunlan.java.basic.learning.floattype;
+
+import org.junit.Test;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 10:24 AM 2018/09/28.
+ */
+public class FloatValueTest {
+    @Test
+    public void test1(){
+        float a = 1.33333F;
+
+    }
+}
