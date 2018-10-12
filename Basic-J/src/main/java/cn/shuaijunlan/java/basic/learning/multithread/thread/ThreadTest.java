5f87f64fac68cb55046963de8779f3ee5333eabf
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-12 21:37:32 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/multithread/thread/ThreadTest.java
@@ -0,0 +1,14 @@
+package cn.shuaijunlan.java.basic.learning.multithread.thread;
+
+import org.junit.Test;
+
+/**
+ * @author Junlan Shuaijunlan[shuaijunlan@gmail.com].
+ * @since Created in 9:25 PM 10/12/18.
+ */
+public class ThreadTest {
+    @Test
+    public void test1(){
+        Thread thread = new Thread();
+    }
+}
