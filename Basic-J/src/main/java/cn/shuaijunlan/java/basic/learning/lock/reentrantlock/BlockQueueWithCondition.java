dda1836e52e981329f3baac0c3be78a6287526ee
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-16 16:20:23 +0800

    update

--- Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/lock/reentrantlock/BlockQueueWithCondition.java
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/lock/reentrantlock/BlockQueueWithCondition.java
@@ -1,5 +1,7 @@
 package cn.shuaijunlan.java.basic.learning.lock.reentrantlock;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -7,8 +9,16 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Junlan Shuai[shuaijunlan@gmail.com].
  * @date Created on 7:43 PM 2018/10/02.
  * The class will achieve an blocking queue basing on {@link ReentrantLock} and {@link Condition},
+ *
+ * 2018-10-16 16:04:07 INFO  cn.shuaijunlan.java.basic.learning.lock.reentrantlock.BlockQueueWithCondition     - count: -1202168
+ * 2018-10-16 16:04:07 INFO  cn.shuaijunlan.java.basic.learning.lock.reentrantlock.BlockQueueTest     - 991
+ * 2018-10-16 16:04:07 INFO  cn.shuaijunlan.java.basic.learning.lock.reentrantlock.BlockQueueWithCondition     - count: -1202169
  * and providing take() and put() methods.
+ *
+ *
+ * Why not it is thread security?
  */
+@Slf4j
 public class BlockQueueWithCondition<T> {
 
     private ReentrantLock lock = new ReentrantLock();
@@ -35,7 +45,7 @@ public class BlockQueueWithCondition<T> {
         lock.lock();
         try {
             if (count.equals(maxLength)){
-                System.out.println("The queue is full!");
+                log.info("The queue is full!");
                 full.await();
             }
             putIndex = putIndex % maxLength;
@@ -58,11 +68,14 @@ public class BlockQueueWithCondition<T> {
             takeIndex = takeIndex % maxLength;
             val = value[takeIndex++];
             count--;
+            if (count < 0){
+                log.info("count: {}", count);
+                System.exit(1);
+            }
             full.signalAll();
         }finally {
            lock.unlock();
         }
         return (T) val;
     }
-
 }
