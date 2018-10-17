717af41b9fde82c648230eebebbff82f9d4662ee
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-17 17:53:26 +0800

    update

--- Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/concurrent/blockingqueue/BlockQueueTest.java
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/concurrent/blockingqueue/BlockQueueTest.java
@@ -1,6 +1,5 @@
 package cn.shuaijunlan.java.basic.learning.concurrent.blockingqueue;
 
-import cn.shuaijunlan.java.basic.learning.lock.reentrantlock.BlockQueueWithCondition;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -13,7 +12,7 @@ public class BlockQueueTest {
 
     @Test
     public void test2() throws InterruptedException {
-        CustomBlockQueue<Integer> blockQueueWithCondition = new CustomBlockQueue<>();
+        CustomBlockQueue<Integer> blockQueueWithCondition = new CustomBlockQueue<>(1);
 
         Thread thread1 = new Thread(() -> {
             try {
@@ -48,12 +47,24 @@ public class BlockQueueTest {
                 }
             }
         });
+
+        Thread thread4 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++){
+                try {
+                    blockQueueWithCondition.put(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         thread1.start();
         thread2.start();
         thread3.start();
+        thread4.start();
 
         thread1.join();
         thread2.join();
         thread3.join();
+        thread4.join();
     }
 }
