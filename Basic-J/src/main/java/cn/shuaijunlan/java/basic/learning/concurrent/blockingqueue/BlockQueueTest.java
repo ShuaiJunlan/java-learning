91d90722a6057e40946d5456842863e727834df2
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-17 16:34:51 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/concurrent/blockingqueue/BlockQueueTest.java
@@ -0,0 +1,59 @@
+package cn.shuaijunlan.java.basic.learning.concurrent.blockingqueue;
+
+import cn.shuaijunlan.java.basic.learning.lock.reentrantlock.BlockQueueWithCondition;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 8:04 PM 2018/10/02.
+ */
+@Slf4j
+public class BlockQueueTest {
+
+    @Test
+    public void test2() throws InterruptedException {
+        CustomBlockQueue<Integer> blockQueueWithCondition = new CustomBlockQueue<>();
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                while (true){
+                    if (log.isInfoEnabled()){
+                        log.info(blockQueueWithCondition.take().toString());
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                while (true){
+                    if (log.isInfoEnabled()){
+                        log.info(blockQueueWithCondition.take().toString());
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            for (int i = 0; i < 1000; i++){
+                try {
+                    blockQueueWithCondition.put(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+    }
+}
---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/concurrent/blockingqueue/CustomBlockQueue.java
@@ -0,0 +1,77 @@
+package cn.shuaijunlan.java.basic.learning.concurrent.blockingqueue;
+
+import lombok.extern.slf4j.Slf4j;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author Junlan Shuai[shuaijunlan@gmail.com].
+ * @date Created on 7:43 PM 2018/10/02.
+ * The class will achieve an blocking queue basing on {@link ReentrantLock} and {@link Condition},
+ *
+ */
+@Slf4j
+@ThreadSafe
+public class CustomBlockQueue<T> {
+
+    private ReentrantLock lock = new ReentrantLock();
+
+    private Condition full = lock.newCondition();
+    private Condition empty = lock.newCondition();
+
+    private Integer maxLength = 1 << 4;
+
+    private Integer putIndex = 0, takeIndex = 0;
+    private Integer count = 0;
+
+    private Object[] value;
+
+    public CustomBlockQueue(){
+        value = new Object[maxLength];
+    }
+
+    public CustomBlockQueue(Integer maxLength){
+        this.maxLength = maxLength;
+        value = new Object[maxLength];
+    }
+
+    public void put(T val) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count.equals(maxLength)){
+                log.info("The queue is full!");
+                full.await();
+            }
+            putIndex = putIndex % maxLength;
+            value[putIndex++] = val;
+            count++;
+            empty.signal();
+        }finally {
+            lock.unlock();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public T take() throws InterruptedException {
+        lock.lock();
+        Object val;
+        try {
+            while (count == 0){
+                empty.await();
+            }
+            takeIndex = takeIndex % maxLength;
+            val = value[takeIndex++];
+            count--;
+            if (count < 0){
+                log.info("count: {}", count);
+                System.exit(1);
+            }
+            full.signal();
+        }finally {
+           lock.unlock();
+        }
+        return (T) val;
+    }
+}
