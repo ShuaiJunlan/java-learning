29e585e6b6986315a15962a73214b5d0019eb438
Shuai Junlan <shuaijunlan@gmail.com>
2018-10-20 15:33:51 +0800

    update

--- netty/src/main/java/cn/shuaijunlan/netty/bytebuf/ByteBufTest.java
+++ netty/src/main/java/cn/shuaijunlan/netty/bytebuf/ByteBufTest.java
@@ -4,6 +4,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
 import io.netty.buffer.UnpooledByteBufAllocator;
+import org.junit.Test;
 
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
@@ -13,5 +14,57 @@ public class ByteBufTest {
     public static void main(String[] args) {
         ByteBufAllocator pooledByteBuf = new UnpooledByteBufAllocator(true);
 
+    }
+    @Test
+    public void test1(){
+        ByteBuf buffer = ByteBufAllocator.DEFAULT.buffer(9, 100);
+        print("allocate ByteBuf(9, 100)", buffer);
+
+        // 1.
+        buffer.writeBytes(new byte[]{1, 2, 3, 4});
+        print("writeBytes(1,2,3,4)", buffer);
+
+        //2.
+        buffer.writeInt(12);
+        print("writeInt(12)", buffer);
+
+        //3.
+        buffer.writeBytes(new byte[]{5});
+        print("writeBytes(5)", buffer);
+
+        // write 方法改变写指针，写的时候发现 buffer 不可写则开始扩容，扩容之后 capacity 随即改变
+        buffer.writeBytes(new byte[]{6});
+        print("writeBytes(6)", buffer);
+
+        // get 方法不改变读写指针
+        System.out.println("getByte(3) return: " + buffer.getByte(3));
+        System.out.println("getShort(3) return: " + buffer.getShort(3));
+        System.out.println("getInt(3) return: " + buffer.getInt(3));
+        print("getByte()", buffer);
+
+        // set 方法不改变读写指针
+        buffer.setByte(buffer.readableBytes() + 1, 0);
+        print("setByte()", buffer);
+
+        // read 方法改变读指针
+        byte[] dst = new byte[buffer.readableBytes()];
+        buffer.readBytes(dst);
+        print("readBytes(" + dst.length + ")", buffer);
+
+
+    }
+    private static void print(String action, ByteBuf buffer){
+        System.out.println("after ===================== " + action + " =====================");
+        System.out.println("capacity(): " + buffer.capacity());
+        System.out.println("maxCapacity(): " + buffer.maxCapacity());
+        System.out.println("readerIndex(): " + buffer.readerIndex());
+        System.out.println("readableBytes(): " + buffer.readableBytes());
+        System.out.println("isReadable(): " + buffer.isReadable());
+
+        System.out.println("writerIndex(): " + buffer.writerIndex());
+        System.out.println("writableBytes():" + buffer.writableBytes());
+        System.out.println("isWritable(): " + buffer.isWritable());
+        System.out.println("maxWritableBytes(): "+ buffer.maxWritableBytes() );
+        System.out.println();
     }
 }
