a8ba3a6db17a0c40ddf88071708b0bd20758de4d
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-06 10:20:29 +0800

    update

--- Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/lock/synchronization/SynchronizationTest.java
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/lock/synchronization/SynchronizationTest.java
@@ -1,5 +1,7 @@
 package cn.shuaijunlan.java.basic.learning.lock.synchronization;
 
+import org.junit.Test;
+
 /**
  * @author Junlan Shuai[shuaijunlan@gmail.com].
  * @since Created in 9:16 AM 11/6/18.
@@ -30,4 +32,29 @@ public class SynchronizationTest {
         }).start();
 
     }
+    @Test
+    public void test1() throws InterruptedException {
+        System.out.println(Thread.currentThread().isDaemon());
+        new Thread(() -> {
+            Object lock1 = lock;
+            synchronized (lock1){
+                try {
+                    Thread.sleep(5000);
+                    System.out.println("Hello lock1!");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        Thread.sleep(1000);
+
+        new Thread(() -> {
+            Object lock2 = lock;
+            synchronized (lock2){
+                System.out.println("Hello lock2!");
+            }
+        }).start();
+    }
+
 }
