3d6aa6807a78f0736e0fc2800272a37716c8d03b
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-07 18:50:02 +0800

    update

--- Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/synchronizer/phaser/PhaserTest.java
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/synchronizer/phaser/PhaserTest.java
@@ -1,5 +1,7 @@
 package cn.shuaijunlan.java.basic.learning.synchronizer.phaser;
 
+import org.junit.Test;
+
 import java.util.Random;
 import java.util.concurrent.Phaser;
 
@@ -24,6 +26,54 @@ public class PhaserTest {
             Thread.sleep(100);
         }
         System.out.println("Game over");
+    }
+    @Test
+    public void test1() throws InterruptedException {
+        final Phaser phaser = new Phaser(0); // default zero
+        for (int index = 1; index <= 10; index++){
+            if (index == 1){
+                Thread.sleep(1000);
+            }
+            // Thread.sleep(1000);
+            System.out.println(phaser.getRegisteredParties() + ":" + phaser.isTerminated());
+            phaser.register();
+            System.out.println(phaser.getRegisteredParties() + ":" + phaser.isTerminated());
+            new Thread(
+                    new Player(phaser),
+                    "Player" + index).start();
+        }
+        System.out.println("Game start");
+        // phaser.arriveAndDeregister();
+
+        while (!phaser.isTerminated()){
+            Thread.sleep(100);
+        }
+        System.out.println("Game over");
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+        Phaser phaser = new Phaser();
+        System.out.println(phaser.isTerminated());
+        phaser.forceTermination();
+        System.out.println(phaser.isTerminated());
+        phaser.register();
+        System.out.println(phaser.isTerminated() + ":" + phaser.getRegisteredParties());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    System.out.println("finishing task!");
+                    phaser.arriveAndDeregister();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        while (!phaser.isTerminated()){
+            Thread.sleep(100);
+        }
     }
 }
 class Player implements Runnable{
