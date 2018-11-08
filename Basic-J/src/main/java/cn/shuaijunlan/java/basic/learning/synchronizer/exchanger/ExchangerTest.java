2dcf465450b5147dad1ca6e6c32ed88abf09c7c7
Shuai Junlan <shuaijunlan@gmail.com>
2018-11-08 17:12:39 +0800

    udpate

--- Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/synchronizer/exchanger/ExchangerTest.java
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/synchronizer/exchanger/ExchangerTest.java
@@ -1,5 +1,6 @@
 package cn.shuaijunlan.java.basic.learning.synchronizer.exchanger;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Exchanger;
 
 /**
@@ -8,23 +9,32 @@ import java.util.concurrent.Exchanger;
  */
 public class ExchangerTest {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(1);
         Exchanger<String> exchanger = new Exchanger<>();
 
-        new ExchangeThreadName("1", exchanger).start();
-        new ExchangeThreadName("2", exchanger).start();
+        new ExchangeThreadName("1", exchanger, countDownLatch).start();
+        new ExchangeThreadName("2", exchanger, countDownLatch).start();
+        new ExchangeThreadName("3", exchanger, countDownLatch).start();
+        countDownLatch.countDown();
+
+        exchanger.exchange("Only one");
     }
 }
 class ExchangeThreadName extends Thread{
     private String mark;
     private Exchanger<String> exchanger;
-    ExchangeThreadName(String mark, Exchanger<String> exchanger){
+    private CountDownLatch countDownLatch;
+    ExchangeThreadName(String mark, Exchanger<String> exchanger, CountDownLatch countDownLatch){
         this.mark = mark;
         this.exchanger = exchanger;
+        this.countDownLatch = countDownLatch;
     }
     @Override
     public void run() {
         try {
+            countDownLatch.await();
             System.out.println(mark + ":" + Thread.currentThread().getName());
             String val = exchanger.exchange(Thread.currentThread().getName());
             System.out.println(mark + ":" + val);
