3b8d9966b9b4e7e74eca7b7308cd56316adfca4c
Junlan Shuai <shuaijunlan@gmail.com>
2018-11-19 10:55:24 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/lock/reentrantreadwritelock/ReadWriteLockTest.java
@@ -0,0 +1,18 @@
+package cn.shuaijunlan.java.basic.learning.lock.reentrantreadwritelock;
+
+import org.junit.Test;
+
+/**
+ * @author Shuai Junlan[shuaijunlan@gmail.com].
+ * @since Created in 10:52 AM 11/19/18.
+ */
+public class ReadWriteLockTest {
+    /**
+     * Achieving a read-write lock basing on waiting-notify mechanism before jdk1.5
+     * TODO: Reference https://blog.csdn.net/u011638883/article/details/18605761 / http://tutorials.jenkov.com/java-concurrency/read-write-locks.html
+     */
+    @Test
+    public void test1(){
+
+    }
+}
