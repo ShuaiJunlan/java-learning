7968607f5ffe71c802dca891aaff74195cfabf1a
Junlan Shuai <shuaijunlan@gmail.com>
2018-11-26 10:07:10 +0800

    add oio demo

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/io/oio/IOClient.java
@@ -0,0 +1,37 @@
+package cn.shuaijunlan.java.basic.learning.io.oio;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Shuai Junlan[shuaijunlan@gmail.com].
+ * @since Created in 9:51 AM 11/26/18.
+ */
+public class IOClient {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        executor.execute(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true){
+                    try {
+                        socket.getOutputStream().write((new Date() + " :hello world!").getBytes());
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/io/oio/IOServer.java
@@ -0,0 +1,49 @@
+package cn.shuaijunlan.java.basic.learning.io.oio;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Shuai Junlan[shuaijunlan@gmail.com].
+ * @since Created in 9:51 AM 11/26/18.
+ */
+public class IOServer {
+    public static void main(String[] args) {
+        try {
+            final ServerSocket serverSocket = new ServerSocket(8000);
+            ExecutorService executor = Executors.newCachedThreadPool();
+            executor.execute(() -> {
+                while (true){
+                    try {
+                        Socket socket = serverSocket.accept();
+
+                        new Thread(() -> {
+                            int len;
+                            byte[] data = new byte[1024];
+                            try {
+                                InputStream inputStream =socket.getInputStream();
+
+                                while ((len = inputStream.read(data)) != -1){
+                                    System.out.println(new String(data, 0, len));
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                        }).start();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
