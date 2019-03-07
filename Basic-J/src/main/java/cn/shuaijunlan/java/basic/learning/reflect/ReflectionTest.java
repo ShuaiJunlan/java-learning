b99fe191ff80dc0f20edbbd8f493a50bdcea1a46
Junlan Shuai <shuaijunlan@gmail.com>
2019-03-07 19:44:18 +0800

    update

---
+++ Basic-J/src/main/java/cn/shuaijunlan/java/basic/learning/reflect/ReflectionTest.java
@@ -0,0 +1,11 @@
+package cn.shuaijunlan.java.basic.learning.reflect;
+
+/**
+ * @author Shuai Junlan[shuaijunlan@gmail.com].
+ * @since Created in 6:53 PM 3/7/19.
+ *
+ * TODO: https://www.sczyh30.com/posts/Java/java-reflection-1/
+ *  https://www.sczyh30.com/posts/Java/java-reflection-2/
+ */
+public class ReflectionTest {
+}
