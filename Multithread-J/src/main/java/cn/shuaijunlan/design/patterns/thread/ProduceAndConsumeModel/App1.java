5ec8a0060c4aaa79787f93fc00b382ac03995228
Junlan Shuai <shuaijunlan@gmail.com>
2019-03-08 11:08:25 +0800

    Revert "update"
    This reverts commit 7993c32378da13c7cf1990af8c446f82fba4a360.

--- Multithread-J/src/main/java/cn/shuaijunlan/design/patterns/thread/ProduceAndConsumeModel/App1.java
+++
@@ -1,9 +0,0 @@
-package cn.shuaijunlan.design.patterns.thread.ProduceAndConsumeModel;
-
-/**
- * @author Junlan Shuai[shuaijunlan@gmail.com].
- * @date Created on 18:20 2017/4/12.
- */
-public interface App1 {
-    void getName();
-}
--- Spring/src/main/java/cn/shuaijunlan/design/patterns/interfaces/IOutputGenerator.java
+++
@@ -1,9 +0,0 @@
-package cn.shuaijunlan.design.patterns.interfaces;
-
-/**
- * Created by Mr SJL on 2016/10/27.
- */
-public interface IOutputGenerator
-{
-    void generateOutput();
-}
