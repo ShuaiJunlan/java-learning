3601e7813829996f0a8205ac7f8b48ca131fbb3b
Shuai Junlan <shuaijunlan@gmail.com>
2019-09-27 16:22:45 +0800

    update

---
+++ Basic/src/main/java/cn/shuaijunlan/java/basic/learning/hashcode/User.java
@@ -0,0 +1,15 @@
+package cn.shuaijunlan.java.basic.learning.hashcode;
+
+/**
+ * @author Shuai Junlan[shuaijunlan@gmail.com].
+ * @since Created in 8:57 AM 9/25/19.
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public User(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+}
