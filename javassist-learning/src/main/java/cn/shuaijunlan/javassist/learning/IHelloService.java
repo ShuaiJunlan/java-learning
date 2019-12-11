8a1bd03ab716aaea4f09d0e3d610fa3ecb84e855
Shuai Junlan <shuaijunlan@gmail.com>
2019-12-11 15:18:33 +0800

    update

--- javassist-learning/src/main/java/cn/shuaijunlan/javassist/learning/IHelloService.java
+++ javassist-learning/src/main/java/cn/shuaijunlan/javassist/learning/IHelloService.java
@@ -6,7 +6,7 @@ import java.util.List;
  * @author Shuai Junlan[shuaijunlan@gmail.com].
  * @since Created in 8:14 PM 1/6/19.
  */
-public interface helloService {
+public interface IHelloService {
     void say(String msg);
 
     String echo(String msg);
